0979f4f673609e46b9f739ced7483a91fb68bc4a
Felix Meurer <f.meurer@telekom.de>
2022-02-14 12:56:39 +0100

    created main class WebbankingApplication

---
+++ src/main/java/de/telekom/sea7/impl/model/WebbankingApplication.java
@@ -0,0 +1,12 @@
+package de.telekom.sea7.impl.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbankingApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WebbankingApplication.class, args);
+	}
+}
