4faa58f7ce4d81cbac3efd1d609f1edecfa73931
Felix Meurer <f.meurer@telekom.de>
2022-02-21 13:23:39 +0100

    Implemented Thymeleaf page redirections, replaced js with thymeleaf

--- src/main/java/de/telekom/sea7/impl/view/TransactionListViewImpl.java
+++ src/main/java/de/telekom/sea7/impl/view/TransactionListViewImpl.java
@@ -21,38 +21,5 @@ public class TransactionListViewImpl {
 		model.addAttribute("transactions", transactionList);
 		return "index";
 	}
-	
-//	@GetMapping("/data/transactions.json")
-//	@ResponseBody
-//	public String getJson() {
-//		String json = "{ \"transactions\" : [";
-//		for (Transaction transaction : transactionList) {
-//			json = json.concat("{");
-//			json = json.concat("\"id\" : ");
-//			json = json.concat(Integer.toString(transaction.getId()));
-//			json = json.concat(",");
-//			json = json.concat("\"receiver\" : \"");
-//			json = json.concat(transaction.getReceiver());
-//			json = json.concat("\",");
-//		    json = json.concat("\"iban\" : \"");
-//			json = json.concat(transaction.getIban());
-//			json = json.concat("\",");
-//			json = json.concat("\"bic\" : \"");
-//			json = json.concat(transaction.getBic() );
-//			json = json.concat("\",");
-//			json = json.concat("\"purpose\" : \"");
-//			json = json.concat(transaction.getPurpose() );
-//			json = json.concat("\",");
-//			json = json.concat("\"amount\" : " );
-//			json = json.concat(Float.toString(transaction.getAmount()));
-//			json = json.concat("}");
-//			if (transactionList.indexOf(transaction) < transactionList.size() - 1) {
-//				json = json.concat(",");
-//			}
-//		}
-//		json = json.concat("]}");
-//		return json;
-//	}
-	
 
 }
--- src/main/java/de/telekom/sea7/impl/view/TransactionViewImpl.java
+++ src/main/java/de/telekom/sea7/impl/view/TransactionViewImpl.java
@@ -1,6 +1,7 @@
 package de.telekom.sea7.impl.view;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,35 +17,14 @@ public class TransactionViewImpl {
 	TransactionList transactionList;
 	
 	@GetMapping("/transaction")
-	public String getTransactionHtml(/*@RequestParam int transaction*/) {
+	public String getTransactionHtml(@RequestParam int transaction, Model model) {
+		model.addAttribute("transaction", transactionList.get(transaction - 1));
 		return "transaction";
 	}
 	
-	@GetMapping("data/{id}/oneTransaction.json")
-	@ResponseBody
-	public String getTransaction(@PathVariable int id) {
-		Transaction transaction = transactionList.get(id - 1);
-		String json = "{";
-		json = json.concat("\"id\" : ");
-		json = json.concat(Integer.toString(transaction.getId()));
-		json = json.concat(",");
-		json = json.concat("\"receiver\" : \"");
-		json = json.concat(transaction.getReceiver());
-		json = json.concat("\",");
-	    json = json.concat("\"iban\" : \"");
-		json = json.concat(transaction.getIban());
-		json = json.concat("\",");
-		json = json.concat("\"bic\" : \"");
-		json = json.concat(transaction.getBic() );
-		json = json.concat("\",");
-		json = json.concat("\"purpose\" : \"");
-		json = json.concat(transaction.getPurpose() );
-		json = json.concat("\",");
-		json = json.concat("\"amount\" : " );
-		json = json.concat(Float.toString(transaction.getAmount()));
-		json = json.concat("}");
-		
-		return json;
+	@GetMapping("/newTransaction")
+	public String getNewTransaction() {
+		return "newTransaction";
 	}
-
+	
 }
